06fd2910cf3de327321a879d58e2dd70b787db4c
amerenda <alexm@moove.ai>
2024-06-08 15:18:12 -0400

    fixes things

--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -3,11 +3,11 @@
 # Function to renew the certificate every 12 hours
 renew_certificate() {
   while true; do
-    certbot certonly --standalone -d "$JELLYFIN_URL" -n
+    certbot -v certonly --standalone -d "$JELLYFIN_URL" -n || true
     sleep 12h
   done
 }
 
 # Start renewing the certificate
 renew_certificate
-
+#certbot -v certonly --standalone -d "$JELLYFIN_URL" -n
